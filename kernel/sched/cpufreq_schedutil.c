2229cb51a32277795df90dd7f7233d945002e524
at1600 <64513159+at1600@users.noreply.github.com>
2026-07-04 17:04:32 +0800

    Update cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -8,7 +8,7 @@
 
 #include <trace/hooks/sched.h>
 
-#define IOWAIT_BOOST_MIN	(SCHED_CAPACITY_SCALE / 8)
+#define IOWAIT_BOOST_MIN	(SCHED_CAPACITY_SCALE / 12)
 
 struct sugov_tunables {
 	struct gov_attr_set	attr_set;
